afd3e4c8a62826e4c9553d2640497d9c4fd6392d
youdonut <nodajeperson4@gmail.com>
2023-02-26 23:52:11 +0800

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,5 @@
+import * as bootstrap from 'bootstrap'
+import * as confetti from './confetti'
+
+console.log(`It's working 😘`)
+document.addEventListener('DOMContentLoaded',confetti.startConfetti())
